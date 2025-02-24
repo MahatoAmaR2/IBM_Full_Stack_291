d9470864ee74d18003ae64b076e8a731660b0183
MahatoAmaR2 <bauaa476@gmail.com>
2025-02-24 15:51:44 +0530

    Day-10

---
+++ Day-10/Class_work/script.js
@@ -0,0 +1,60 @@
+let str = "Radhe Radhe";
+
+// console.log(str.length);
+// console.log(str.length-1);
+// console.log(str[0]);
+
+let arr = [2, 8, "SVU", 1.2, null, undefined, true, ""];
+// arr[2]="LNCTU"
+// arr.pop();
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// const citrus = fruits.splice(1, 3);
+
+let obj = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  1: "blue",
+  student: {
+    class: "B Tech",
+    roll: 291,
+    subject: ["NM", "RM", "Cn", "DBMS"],
+  },
+};
+// console.log(obj.student);
+// obj.age=28
+
+// let name = "Amar"
+// let Class = "B Tech"
+// let hobbies = ["Games", "Listenin"]
+// let obj1 = {
+//     name, Class, hobbies
+// }
+let arr1 = [1, 2, 3, [2, 4, 5, [5]]];
+
+let superHeroes = {
+  1: {
+    name: "Batman",
+    power: ["intelligence", "money"],
+    health: 45,
+    villains: [
+      { name: "Redhood", health: 34 },
+      { name: "Twoface", health: 64 },
+    ],
+    metadata: { favouriteColor: "red", age: 15 },
+  },
+  2: {
+    name: "Ironman",
+    power: ["jarvis", "money"],
+    health: 33,
+    villains: [
+      { name: "Manderin", health: 44 },
+      { name: "Titanium Man", health: 56 },
+    ],
+    metadata: { favouriteColor: "red", age: 13 },
+  },
+};
+
+// console.log(superHeroes[1].metadata.age);
+console.log(obj[1]);
+
