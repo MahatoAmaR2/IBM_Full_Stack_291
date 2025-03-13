e539a811d38556f7aaddfcf79d35682627175771
MahatoAmaR2 <bauaa476@gmail.com>
2025-03-13 15:49:02 +0530

    Day-23

---
+++ Day-23/Classwork/Backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const userRouter = require("./routes/user.route.js");
+const app = express();
+app.use(express.json());
+
+const PORT = 8080;
+app.get("/", (req, res) => {
+  res.status(200).send(`<h1>Har Har Mahadev Day23</h1>`);
+});
+
+app.use(userRouter)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
---
+++ Day-23/Classwork/Backend/routes/user.route.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { signup, login } = require("../controllers/user.controller.js");
+
+const userRouter = express.Router();
+
+userRouter.post("/signup", signup);
+userRouter.post("/login", login);
+
+module.exports = userRouter;
